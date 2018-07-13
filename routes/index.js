70f3a6109cbd25c7c399239fd29a7e7b4d11be1e
equilibriumwell <equilibriumwell@aol.com>
2018-07-13 16:39:05 -0400

    trying to figure the view

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+const sqlite = require('sqlite3').verbose();
+const models = require('../models');
+const sequelize = require('sequelize');
+const Op = sequelize.Op;
+
+/* GET home page. */
+router.post('/playlists', (req, res) => {
+  models.playlists
+    .findOrCreate({
+      where: {
+        Name: req.body.name,
+        DateFormed: req.body.dateFormed
+      }
+    })
+    .spread(function(result, created) {
+      if (created) {
+        res.redirect('/playlists');
+      } else {
+        res.send('This playlist already exists!');
+      }
+    });
+});
+
+router.get('/playlists', function(req, res, next) {
+  models.artists.findAll({}).then(playlistsFound => {
+    res.render('playlists', {
+      playlists: playlistsFound
+    });
+  });
+});
+
+module.exports = router;
